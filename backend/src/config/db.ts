05dfca11c4117a3a654c84154ac2358a3966d45f
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-11-12 15:06:04 +0530

    url fallback

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 // Function to connect to MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(String(process.env.MONGO_URI));
+    const conn = await mongoose.connect(
+      String(process.env.MONGO_URI || "mongodb://localhost:27017/forminit")
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error: unknown) {
     if (error instanceof Error) {
